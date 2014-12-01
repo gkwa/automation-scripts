b76af03218aec77e9614613114ea6d3e3816075b
mcennedy <mcennedy@konsequence.net>
2014-12-01 17:55:00 +0100

    pol cl admin, dektop

--- Puppet/polaris/client/install-admin.pp
+++ Puppet/polaris/client/install-admin.pp
@@ -1,8 +1,6 @@
 include chocolatey_sw
 
 
-
-
 package { 'driverbooster':
   ensure   => present,
   provider => 'chocolatey',
@@ -41,72 +39,22 @@ package { 'Revo.Uninstaller':
   require  => Class['chocolatey_sw'],
 }
 
-/**/
 
+package { '7zip':
+  ensure   => present,
+  provider => 'chocolatey',
+  require  => Class['chocolatey_sw'],
+}
 
-/*
+package { 'KatMouse':
+  ensure   => present,
+  provider => 'chocolatey',
+  require  => Class['chocolatey_sw'],
+}
 
-doesnt run, hangs after startup
-
-*/
-
-# package { 'ChocolateyGUI':
-#   ensure   => absent,
-#   provider => 'chocolatey',
-#   require  => Class['chocolatey_sw'],
-# }
-
-
-/*
-
-DebugView is an application that lets you monitor debug output on your local system, or any computer on the network that you can reach via TCP/IP. It is capable of displaying both kernel-mode and Win32 debug output, so you don't need a debugger to catch the debug output your applications or device drivers generate, nor do you need to modify your applications or drivers to use non-standard debug output APIs.
-
-*/
-
-# package { 'DebugView':
-#   ensure   => present,
-#   provider => 'chocolatey',
-#   require  => Class['chocolatey_sw'],
-# }
-
-
-/*
-
-DefaultProgramsEditor
-Default Programs Editor 2.7.2675.2253
-By: bdukes
-Default Programs Editor makes it easy to fix file association settings in Windows, including context menu items, icons, and descriptions. Also: autoplay settings and default programs settings ...all without fiddling around in the registry.
-
-*/
-
-# package { 'DefaultProgramsEditor':
-#   ensure   => present,
-#   provider => 'chocolatey',
-#   require  => Class['chocolatey_sw'],
-# }
-
-/*
-
-Programmers Notepad - Windows programming editor with an integrated Hex viewer/editor. | Please install with chocolatey (http://nuget.org/List/Packages/chocolatey).
-
-*/
-
-# package { 'ProgrammersNotepad':
-#   ensure   => absent,
-#   provider => 'chocolatey',
-#   require  => Class['chocolatey_sw'],
-# }
-
-
-/*
-
-__HostsMan__ is a freeware application that lets you manage your Hosts file with ease. No Spyware! No Adware! No Viruses! 100% Freeware! Clean and simple.
-
-*/
-
-# package { 'hostsman':
-#   ensure   => present,
-#   provider => 'chocolatey',
-#   require  => Class['chocolatey_sw'],
-# }
+package { 'launchy':
+  ensure   => present,
+  provider => 'chocolatey',
+  require  => Class['chocolatey_sw'],
+}
 
