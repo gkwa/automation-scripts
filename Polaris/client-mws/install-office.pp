7042788fae2ac25b17a43271c7c90eb3eeca5dee
mcennedy <mcennedy@konsequence.net>
2014-12-02 16:11:52 +0100

    mws

--- Polaris/client-mws/install-office.pp
+++ Polaris/client-mws/install-office.pp
@@ -26,35 +26,6 @@ package { 'ganttproject':
   require  => Class['chocolatey_sw'],
 }
 
-/*
-
-WinImage is a fully-fledged disk-imaging suite for easy creation, 
-reading and editing of many image formats and fileystems, including DMF, VHD, FAT, ISO, NTFS and Linux. 
-The disk image is an exact copy of a physical disk (floppy, CD-ROM, hard disk, USB, VHD disk, etc.) 
-or a partition that preserves the original structure. 
-With WinImage in place, you can recreate the disk image on the hard drive or other media, view its content, 
-extract image-based files, add new files and directories, change the format, and defragment the image. 
-All this and more is delivered in one intuitive user interface that enables imaging right out of the box.
-
-The program has many utilitarian uses at home and in the office. 
-As a serious PC user, you probably have tons of old but still useful floppy disks. 
-With WinImage in place, you can turn them into disk images, which can be stored on the hard drive 
-and recreated, when a need arises. In combination with a CD creating tool, 
-WinImage can help you create your own custom boot disk with hardware diagnostic or virus cleaning software 
-to bring a problem PC back up and running without being in Windows. As a hard-disk backup solution, 
-WinImage allows you to save hours and even days restoring a system and configurations on a machine 
-that has experienced a hard-disk crash or software corruption. 
-Along with homes and offices, this ability is a must for training classes, 
-where restoring torn down PC configurations quickly is critical.
-
-*/
-
-
-package { 'WinImage':
-  ensure   => present,
-  provider => 'chocolatey',
-  require  => Class['chocolatey_sw'],
-}
 
 
 
